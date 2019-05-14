0e0cb2f59d7ebdebad33af62fe605f6fe5418439
zhoufanglu <735417031@qq.com>
2019-05-14 16:17:20 +0800

    rename

---
+++ client/fronted/src/page/home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div>home</div>
+        <router-link to="/test">
+            <h3>test</h3>
+        </router-link>
+    </div>
+</template>
+<script>
+  export default {
+    name: '',
+    data(){
+      return{
+
+      }
+    },
+    methods:{
+
+    },
+    mounted(){
+      [3, 15, 8, 29, 102, 22].sort((a,b) => {
+        return a -b
+      });
+    }
+  }
+</script>
+<style>
+</style>
