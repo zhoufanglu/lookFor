9009ed39738ff474c41d6c3dffb44dac9f55d4fa
卢方舟 <735417031@qq.com>
2020-08-17 16:21:14 +0800

    feat:add pic show && pic lazy

--- client/fronted/src/page/tool/children/imgShow.vue
+++ client/fronted/src/page/tool/children/imgShow.vue
@@ -4,12 +4,39 @@
       <li
           v-for="(i,index) in imgList"
           :key="index"
+          @click="showImg(i)"
       >
-        <img
-            :src="`${url.fileServer}/img/${i.name}`"
+        <van-checkbox v-model="i.isCheck"></van-checkbox>
+
+        <van-image
+            round
+            width="100"
+            height="100"
+            :src="i.path"
+            lazy-load
+            fit="cover"
         >
+          <template v-slot:loading>
+            <van-loading type="spinner" size="20" />
+          </template>
+          <template v-slot:error>加载失败</template>
+        </van-image>
+
+
+        <!--<img
+            :src="i.path"
+        >-->
+
       </li>
     </ul>
+
+    <van-popup v-model="popUp.show">
+      <van-image
+          round
+          :src="popUp.img.path"
+      />
+    </van-popup>
+
   </div>
 </template>
 <script>
@@ -20,33 +47,69 @@
     data() {
       return {
         imgList: [],
-        url: ''
+        url: '',
+        popUp: {
+          show: false,
+          img: {}
+        }
       }
     },
     created() {
       this.url = base
-      this.$nextTick(()=>{
-        this.getImgNameList()
-      })
+      this.getImgNameList()
     },
     mounted() {
     },
     methods: {
       async getImgNameList() {
         const arr = await this.$api.tool.getImgNameList(100)
-        this.imgList = arr.data.data
-        this.imgList.forEach(img=>{
-          console.log(18, `${base.nodeServer}/img/${img.name}`)
+        const imgList = arr.data.data
+        imgList.forEach((img, index)=>{
+          this.imgList.push(
+            {
+              id: img.id,
+              isCheck: false,
+              path: `${this.url.fileServer}/img/${img.name}`
+            }
+          )
         })
+        this.imgList.forEach((i,index)=>{
+          this.$set(this.imgList, index, i)
+        })
+      },
+      showImg(img) {
+        console.log(img)
+        this.popUp.show = true
+        this.popUp.img = img
       }
     }
   }
 </script>
 <style lang="scss" scoped>
   .p-img-show {
+    ul{
+      margin-top: 10px;
+      li{
+        @include vertical-center;
+        justify-content: flex-start;
+        padding-left: 10px;
+        cursor: pointer;
+        .van-checkbox{
+          padding: 10px;
+        }
+      }
+    }
     img{
       width: 100px;
       height: 100px;
+    }
+  }
+</style>
+
+<style lang="scss">
+  .p-img-show{
+    .van-popup{
+      background-color:transparent;
     }
   }
 </style>
